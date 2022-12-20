ebbd8e6b8fa294a58497a5825237f1aa98df1019
Kocheng <devil871018@gmail.com>
2022-12-20 15:11:55 +0800

    [add] mongodb query example

--- src/main/java/com/kocheng/demo1/repository/ProductRepostory.java
+++ src/main/java/com/kocheng/demo1/repository/ProductRepostory.java
@@ -4,12 +4,33 @@ import java.util.List;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.kocheng.demo1.entity.Product;
 
-@Repository
 public interface ProductRepostory extends MongoRepository<Product, String> {
 
-    List<Product> findByPriceBetweenAndNameLikeIgnoreCase(int priceForm, int priceTo, String keyword, Sort sort);
+    @Query("{'$and': [{'price': {'$gte': ?0, '$lte': ?1}}, {'name': {'$regex': ?2, '$options': 'i'}}]}")
+    List<Product> findByPriceBetweenAndNameLikeIgnoreCase(int priceFrom, int priceTo, String name, Sort sort);
+
+    @Query("{'price': {'$gte': ?0, '$lte': ?1}}")
+    List<Product> findByPriceBetween(int from, int to);
+
+    @Query("{'name': {'$regex': ?0, '$options': 'i'}}")
+    List<Product> findByNameLikeIgnoreCase(String name);
+
+    @Query("{'$and': [{'price': {'$gte': ?0, '$lte': ?1}}, {'name': {'$regex': ?2, '$options': 'i'}}]}")
+    List<Product> findByPriceBetweenAndNameLikeIgnoreCase(int priceFrom, int priceTo, String name);
+
+    @Query(value = "{'_id': {'$in': ?0}}", count = true)
+    int countByIdIn(List<String> ids);
+
+    @Query(value = "{'_id': {'$in': ?0}}", exists = true)
+    boolean existsByIdIn(List<String> ids);
+
+    @Query(delete = true)
+    void deleteByIdIn(List<String> ids);
+
+    @Query(sort = "{'name': 1, 'price': -1}")
+    List<Product> findByIdInOrderByNameAscPriceDesc(List<String> ids);
 }
